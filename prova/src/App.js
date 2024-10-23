20704cf554d8f56ae21905437bab34d0133e55bc
mahbjr <macontato2002@gmail.com>
2024-10-23 14:01:52 -0300

    commit ponto

--- prova/src/App.js
+++ prova/src/App.js
@@ -2,6 +2,8 @@ import Questao01A from "./components/Questao01";
 //import Questao02 from "./components/Questao02";
 //import Questao03 from "./components/Questao03";
 //import Questao04 from "./components/Questao04";
+//ow yeah
+//ainn
 
 function App() {
   return (
